8d7a7fd46816da19457b15fe1d976f57cd997d29
irstmon <24887336+irstmon@users.noreply.github.com>
2026-08-20 09:58:09 +0200

    Fix HTML structure and clean up commented code in menu templates
    - Corrected the closing tag for the "Home" link in menu_start.html.
    - Removed unnecessary comment markers in menu_admin_begin.html, menu_admin_end.html, and menu_user_end.html for cleaner code.

--- include/js/jquery_plugins/jquery.nconf_accordion_list.js
+++ include/js/jquery_plugins/jquery.nconf_accordion_list.js
@@ -1,15 +1,15 @@
 //
 // accordion enhanced
 //
-(function($){
-    $.fn.nconf_accordion_list = function(event) {
-        return $(this).each( function() {
+(function ($) {
+    $.fn.nconf_accordion_list = function (event) {
+        return $(this).each(function () {
 
             // accordion style enhanced to handle all items
-            if (event.type == 'click'){
+            if (event.type == 'click') {
                 var clicked_element = $(this);
-                clicked_element.toggleClass("ui-state-active ui-corner-bottom").next(".accordion_content").toggle("blind", function(){
-                   clicked_element.children("span").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s");
+                clicked_element.toggleClass("ui-state-active ui-corner-bottom").next(".accordion_content").toggle("blind", function () {
+                    clicked_element.children("span").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s");
                 });
                 return false;
             } else {
@@ -21,23 +21,23 @@
 
     // NConf's clever accordion
     // it saves the state as cookie and can so know the state of an element after refresh or revisiting the page
-    $.fn.nconf_accordion_clever = function(event) {
-        return $(this).each( function() {
+    $.fn.nconf_accordion_clever = function (event) {
+        return $(this).each(function () {
 
             var accordion_id = $(this).attr("id");
             var cookie_status = readCookie(accordion_id);
             if (cookie_status && cookie_status == "closed") {
                 $(this).toggleClass("ui-corner-bottom closed").next().hide();
-            }else{
+            } else {
             }
 
             // Add a click event to open and close menu parts
-            $(this).click(function() {
+            $(this).click(function () {
                 $(this).toggleClass("ui-corner-bottom closed").next().slideToggle('slow');
                 // Save the state as cookie to have a persistent state
-                if ($(this).hasClass("closed") === false){
+                if ($(this).hasClass("closed") === false) {
                     createCookie(accordion_id, "open", 365);
-                }else{
+                } else {
                     createCookie(accordion_id, "closed", 365);
                 }
                 return false;
